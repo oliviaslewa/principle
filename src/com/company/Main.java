97f7af4e566ab9d34c166dd96c46bdfdf15afbff
oliviaslewa <oliviasalm7@gmail.com>
2022-08-16 00:10:44 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,12 @@
 package com.company;
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] array = new int[5];
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("enter the value for num: " + i);
-            array[i] = sc.nextInt();}
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("value of num " + i + " is: " + array[i]);}
-        System.out.println("--------------------------------");
-        int[] Array = {1, 2, 3};
-        System.out.println("sum of array is: " + arraySum(Array));}
-    public static int arraySum(int[] Array) {
-        int sum = 0;
-        for (int i = 0; i < Array.length; i++) {
-            sum = sum + Array[i];}
-        return sum;}}
+        int[] minimumElementInArray= {29,6,3,9,21};
+        System.out.println(minimumNumForArray(minimumElementInArray));}
+public static int minimumNumForArray(int[] min){
+int minimumElement=min[0];
+    for (int i = 0; i < min.length; i++) {
+        if (min[i]<minimumElement){
+minimumElement=min[i];
+
+        }}return minimumElement;}}
--- src/com/company/array.java
+++ src/com/company/array.java
@@ -2,15 +2,18 @@ package com.company;
 import java.util.Scanner;
 public class array {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int[]array=new int[4];
-        System.out.println("enter the numbers: ");
-        for (int i = 0; i < array.length; i++) {
-        array[i]= sc.nextInt();}
-        squaredArray(array);}
-            public static int[] squaredArray(int []array){
-                for (int i = 0; i <4 ; i++) {
-                    array[i]= array[i] * array[i];}
-                for (int i = 0; i < 4; i++) {
-                    System.out.println(array[i]+ " ");}
-                return array;}}
+        Scanner sc = new Scanner(System.in);
+        int[] arrayToFindElement = {1,2,3,4};
+        System.out.println("the element index is: "+searchForItemInArray(arrayToFindElement,2));
+    }
+public static int searchForItemInArray(int [] array, int num){
+    for (int i = 0; i < array.length; i++) {
+        System.out.println("Array index: " + i + " and its element is: " + array[i]);
+        System.out.println("num is: " + num);
+        if (array[i] == num) {
+            System.out.println("founded");
+            return i;
+        }
+    }
+            return -1;
+}}
